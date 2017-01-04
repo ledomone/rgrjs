d13b54b9ca257749288042c320fbd87e63f81c5e
ledomone <ledom.git@gmail.com>
2017-01-04 16:27:56 +0100

    Move component Hello to separate file

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Hello from "./components/Hello";
 
-class Hello extends React.Component {
-  render() {
-    return <h3>Hello React with ES6</h3>;
-  }
-}
-
-// ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
 ReactDOM.render(<Hello />, document.getElementById('react'));
