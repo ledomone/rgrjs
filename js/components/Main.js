801860c95a0d13d513a3e4d28b2308fc44470a07
ledomone <ledom.git@gmail.com>
2017-01-04 22:06:56 +0100

    Create Main React Component

---
+++ js/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Links</h3>
+        <ul>
+          <li>Link ...</li>
+          <li>Link ...</li>
+        </ul>
+      </div>
+    );
+  }
+}
