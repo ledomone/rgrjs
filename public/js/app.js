6abf223e26beb0163fd98d2e18f163c28bb33ffb
ledomone <ledom.git@gmail.com>
2017-01-04 16:17:04 +0100

    Import instead of require

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-let React = require("react");
-let ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
 
 class Hello extends React.Component {
   render() {
-    return <h3>Hello ES6</h3>;
+    return <h3>Hello React with ES6</h3>;
   }
 }
 
