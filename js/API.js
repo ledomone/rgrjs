7db4b7fb34a1ae6c71d5b58166be7a370e22817d
ledomone <ledom.git@gmail.com>
2017-01-04 22:17:59 +0100

    Use jQuery to fetch real data from API

--- js/API.js
+++ js/API.js
@@ -1,7 +1,12 @@
+import {get} from 'jquery';
+
 let API = {
   fetchLinks() {
     console.log('1. In API');
     // Ajax request to read /data/links
+    get('/data/links').done(resp => {
+      console.log(resp);
+    });
   }
 };
 
