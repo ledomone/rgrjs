bb028d13ee3fa10be86965e44ec9fe9987fb9ae2
ledomone <ledom.git@gmail.com>
2017-01-04 19:21:17 +0100

    Add MongoDB support

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from 'express';
 
+import {MongoClient} from 'mongodb';
+
 let app = express();
 
 app.use(express.static('public'));
 
 app.listen(3000);
+
+MongoClient.connect('mongodb://localhost/rgrjs', (err, database) => {
+  if (err) throw err;
+
+  database.collection("links").find({}).toArray((err, links) => {
+    if (err) throw err;
+
+    console.log(links);
+  });
+});
