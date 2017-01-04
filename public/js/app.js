bfe40359e2f2e1814cefd3e768a5671ed4e33ff5
ledomone <ledom.git@gmail.com>
2017-01-04 16:13:39 +0100

    ES6: var -> let, class Hello

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 let React = require("react");
 let ReactDOM = require("react-dom");
 
-let Hello = React.createClass({
-  render() {
-    return <h3>Hello ES6</h3>
-  }
-});
 
+class Hello extends React.Component {
+  render() {
+    return <h3>Hello ES6</h3>;
+  }
+}
 
 // ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
 ReactDOM.render(<Hello />, document.getElementById('react'));
