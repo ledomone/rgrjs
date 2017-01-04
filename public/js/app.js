bedf02f25e7b9ed4c11e80c9fbc4df200aba7569
ledomone <ledom.git@gmail.com>
2017-01-04 14:38:13 +0100

    Use JSX - Babel + Webpack

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,11 @@ var ReactDOM = require("react-dom");
 
 var Hello = React.createClass({
   render: function() {
-    return React.createElement("h3", null, "Hello React!");
+    // return React.createElement("h3", null, "Hello React!");
+    return <h3>Hello Webpack? yeah! </h3>
   }
 });
 
 
-ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
+// ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
+ReactDOM.render(<Hello />, document.getElementById('react'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,15 @@ module.exports = {
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: { presets: ['react', 'es2015', 'stage-0'] }
+      }
+    ]
   }
-}
+};
