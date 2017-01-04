d3339bb2f581fea25e79fef484beb17154703c6a
ledomone <ledom.git@gmail.com>
2017-01-04 22:15:30 +0100

    Load data from API (mock)

---
+++ js/API.js
@@ -0,0 +1,8 @@
+let API = {
+  fetchLinks() {
+    console.log('1. In API');
+    // Ajax request to read /data/links
+  }
+};
+
+export default API;
--- js/components/Main.js
+++ js/components/Main.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import API from '../API';
 
 export default class Main extends React.Component {
+  componentDidMount() {
+    API.fetchLinks();
+  }
+
   render() {
     return (
       <div>
