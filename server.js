271c174f28d658e26580cab392cc02755d0c11cc
ledomone <ledom.git@gmail.com>
2017-01-04 21:49:38 +0100

    Add first API Endpoint for MongoDB Data

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ MongoClient.connect('mongodb://localhost/rgrjs', (err, database) => {
   db = database;
   app.listen(3000, () => console.log('Listening on port 3000'));
 });
+
+app.get("/data/links", (req, res) => {
+    db.collection("links").find({}).toArray((err, links) => {
+      if (err) throw err;
+
+      res.json(links);
+    });
+});
