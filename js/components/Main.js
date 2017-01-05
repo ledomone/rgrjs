1d8992802c4464f2d8089d21211b422b6f67bedf
ledomone <ledom.git@gmail.com>
2017-01-05 13:53:40 +0100

    State as property in the class (STAGE-0)

--- js/components/Main.js
+++ js/components/Main.js
@@ -16,10 +16,11 @@ export default class Main extends React.Component {
     limit: 3
   }
 
+  state = _getAppState();
+
   constructor(props) {
     super(props);
 
-    this.state = _getAppState();
     this.onChange = this.onChange.bind(this);
   }
   componentDidMount() {
