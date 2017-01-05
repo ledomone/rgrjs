4075d1698b26c967d4981293798d53809e0192aa
ledomone <ledom.git@gmail.com>
2017-01-05 13:44:15 +0100

    Property limit omitted - slice function is ok, but all records on the page

--- js/app.js
+++ js/app.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 
 import Main from "./components/Main";
 
-ReactDOM.render(<Main limit={4} />, document.getElementById('react'));
+ReactDOM.render(<Main />, document.getElementById('react'));
