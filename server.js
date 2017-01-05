4fd5d55ae77e7b85e911bc1a9d546d415d2caebc
ledomone <ledom.git@gmail.com>
2017-01-05 14:09:25 +0100

    Async Functions (async - await)

--- server.js
+++ server.js
@@ -7,23 +7,22 @@ let app = express();
 
 app.use(express.static('public'));
 
-let db;
-MongoClient.connect('mongodb://localhost/rgrjs', (err, database) => {
-  if (err) throw err;
+// async anonymous function
+(async () => {
+  let db = await MongoClient.connect('mongodb://localhost/rgrjs');
 
-  db = database;
   app.use('/graphql', GraphQLHTTP({
     schema: schema(db),
     graphiql: true
   }));
 
+  app.get("/data/links", (req, res) => {
+      db.collection("links").find({}).toArray((err, links) => {
+        if (err) throw err;
+
+        res.json(links);
+      });
+  });
+
   app.listen(3000, () => console.log('Listening on port 3000'));
-});
-
-app.get("/data/links", (req, res) => {
-    db.collection("links").find({}).toArray((err, links) => {
-      if (err) throw err;
-
-      res.json(links);
-    });
-});
+})();
